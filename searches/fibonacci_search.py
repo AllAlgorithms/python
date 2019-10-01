0e38f2d61002f8c44679f4d70f40c8144a0e9f1b
Saeed <SaeedEY@users.noreply.github.com>
2019-10-02 00:14:54 +0330

    The Fibonacci Search

---
+++ searches/fibonacci_search.py
@@ -0,0 +1,25 @@
+def fibonacciSearch(lys, val):
+    fibM_minus_2 = 0
+    fibM_minus_1 = 1
+    fibM = fibM_minus_1 + fibM_minus_2
+    while (fibM < len(lys)):
+        fibM_minus_2 = fibM_minus_1
+        fibM_minus_1 = fibM
+        fibM = fibM_minus_1 + fibM_minus_2
+    index = -1;
+    while (fibM > 1):
+        i = min(index + fibM_minus_2, (len(lys)-1))
+        if (lys[i] < val):
+            fibM = fibM_minus_1
+            fibM_minus_1 = fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+            index = i
+        elif (lys[i] > val):
+            fibM = fibM_minus_2
+            fibM_minus_1 = fibM_minus_1 - fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+        else :
+            return i
+    if(fibM_minus_1 and index < (len(lys)-1) and lys[index+1] == val):
+        return index+1;
+    return -1
