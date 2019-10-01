377dbf1b95047711001e4ef7fc44883d99f1ded8
Josh <me@joshl.io>
2019-10-01 18:39:56 +0100

    Fixed case sensitivity bug
    Updated the function to convert the string to uppercase before checking whether it's a palindrome. This prevents it from returning an incorrect value if a string is a palindrome but has inconsistent casing.

--- strings/palindrome.py
+++ strings/palindrome.py
@@ -3,12 +3,14 @@ def reverse(s):
     return s[::-1]
 
 def isPalindrome(s):
+    # Convert s to uppercase to ignore case sensitivity
+    s = s.upper()
     # Checking if both string are equal or not
     if (s == reverse(s)):
         return True
     return False
 
 # Tests
-print(isPalindrome('racecar'))
+print(isPalindrome('Racecar'))
 print(isPalindrome('ferrari'))
-print(isPalindrome('civic'))
+print(isPalindrome('CiVIc'))
