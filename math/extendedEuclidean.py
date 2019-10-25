8cacca6c20eb4279251c66c9981bcd2a7aafdee6
aniakubik <ania.kubik@gmail.com>
2019-10-25 17:38:05 +0200

    fix bug

--- math/extendedEuclidean.py
+++ math/extendedEuclidean.py
@@ -5,16 +5,16 @@
 def extendedEuclidean(a,b):
     a_old, b_old = a,b
     a, b = max(a, b), min(a, b)
-    x, y, old_x, old_y = 0, 1, 1, 0
+    x, y, old_x, old_y = 1, 0, 0, 1
     while b != 0:
         quotient = a // b
         residue = a % b
         a, b = b, residue
-        x, old_x = old_x, (old_x - quotient * x)
-        y, old_y = old_y, (old_y - quotient * y)
-    if a_old > b_old:
-        return x, y
-    return y, x
+        old_x, x = x, (old_x - quotient * x)
+        old_y, y = y, (old_y - quotient * y)
+    if b_old > a_old:
+        return old_x, old_y
+    return old_y, old_x
 
 
  	
