1f4e774f6b2fea031e0d905bcdea9ed7cbbae253
Eduardo Galeano <cegard689@gmail.com>
2020-10-08 20:33:43 -0500

    Fixed working for 0 and negative power values.

--- algorithms/recursion/exponentiation.py
+++ algorithms/recursion/exponentiation.py
@@ -2,17 +2,20 @@
 def exponentiation(baseNumber, power):
     answer = None
     
-    if power == 1:
-        answer = baseNumber
-    
-    elif power == 2:
-        answer = baseNumber * baseNumber
-    
-    else:
+    if power > 1:
         halfAnswer = exponentiation(baseNumber, power//2)
         answer = halfAnswer * halfAnswer
         
         if power%2 == 1:
             answer *= baseNumber
+    
+    elif power == 1:
+        answer = baseNumber
+    
+    elif power == 0:
+        answer = 1
+    
+    else: # negative power
+        answer = 1 / exponentiation(baseNumber, abs(power))
     
     return answer
