fc93e5fafa840de002eaa02ab8070ead2e81f3ec
yashaswiadyalu <yashadyalu@gmail.com>
2020-10-18 21:09:49 +0530

    code for queue

---
+++ data-structures/linked-lists/queue.py
@@ -0,0 +1,30 @@
+def __init__(self):
+    self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def enqueue(self, data):
+        self.items.append(data)
+ 
+    def dequeue(self):
+        return self.items.pop(0)
+ 
+ 
+q = Queue()
+while True:
+    print('enqueue <value>')
+    print('dequeue')
+    print('quit')
+    do = input('What would you like to do? ').split()
+ 
+    operation = do[0].strip().lower()
+    if operation == 'enqueue':
+        q.enqueue(int(do[1]))
+    elif operation == 'dequeue':
+        if q.is_empty():
+            print('Queue is empty.')
+        else:
+            print('Dequeued value: ', q.dequeue())
+    elif operation == 'quit':
+        break
